a277416c3c99246fa7b2e8329f2275acd06ea8dd
francoderico <francoderico2001@gmail.com>
2021-05-27 21:53:22 -0300

    Nada

---
+++ operadores.h
@@ -0,0 +1,22 @@
+#ifndef __OPERADORES_H__
+#define __OPERADORES_H__
+
+
+typedef int (*FuncionEvaluacion)(int *args);
+
+int suma(int *args);
+
+int resta(int *args);
+
+int opuesto(int *args);
+
+int producto(int *args);
+
+int division(int *args);
+
+int modulo(int *args);
+
+int potencia(int *args);
+
+
+#endif /** __OPERADORES_H__ */
