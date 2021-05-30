7cdb4dd3313e5ade85db08a04c634cfd892d0e80
francoderico <francoderico2001@gmail.com>
2021-05-29 22:13:01 -0300

    Updated

--- operadores.c
+++ operadores.c
@@ -63,26 +63,3 @@ int potencia(int *args)
 {
     return pow(args[0], args[1]);   //Chequear esto con los casts y eso.
 }
-
-/*
- * Crea una tabla de operadores vacìa.
-*/
-
-TablaOps tablaOps_crear() {
-  return NULL;
-}
-
-/*
- * Carga un operador a la Tabla de Operadores.
-*/
-void  cargaroperador (TablaOps *tabla, char *simbolo, int aridad , FuncionEvaluacion  eval )
-{
-	Operador *nuevoOperador = malloc(sizeof(Operador));
-	nuevoOperador->simbolo = simbolo;
-	nuevoOperador->aridad = aridad;
-	nuevoOperador->eval = eval;
-	nuevoOperador->sig = tabla;
-}
-
-
-
--- operadores.h
+++ operadores.h
@@ -2,11 +2,6 @@
 #define __OPERADORES_H__
 
 
-
-
-
-typedef int (*FuncionEvaluacion)(int *args);
-
 int suma(int *args);
 
 int resta(int *args);
@@ -21,18 +16,5 @@ int modulo(int *args);
 
 int potencia(int *args);
 
-
-typedef struct Operador {
-  char *simbolo;
-  int aridad;
-  FuncionEvaluacion eval;
-  struct Operador *sig;
-} Operador;
-
-typedef Operador *TablaOps;
-
-TablaOps tablaOps_crear();
-
-void  cargaroperador (TablaOps *tabla, char *simbolo, int aridad , FuncionEvaluacion  eval );
 
 #endif /** __OPERADORES_H__ */
