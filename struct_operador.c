b28c3099758ccd736982b8e968d49c4a82d32c15
francoderico <francoderico2001@gmail.com>
2021-05-29 22:17:12 -0300

    (INCOMPLETO) Implementación de las funciones de la estructura Operador (copiar de la estructira Operacion)

---
+++ struct_operador.c
@@ -0,0 +1,16 @@
+#include "struct_operador.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//Copiar de operacion.c
+
+Operador* operador_crear(char *simbolo, int aridad, FuncionEvaluacion eval)
+{
+    //blabla
+}
+
+
+
+
