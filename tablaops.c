a8c566d91b9430f5ba85d9af9d27b915b613bfb3
francoderico <francoderico2001@gmail.com>
2021-05-29 22:18:41 -0300

    (FALTAN LOS COMENTARIOS) Estructura tablaops

---
+++ tablaops.c
@@ -0,0 +1,58 @@
+#include "glist.h"
+#include "tablaops.h"
+#include "struct_operador.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+TablaOps tablaOps_crear()
+{
+    return glist_crear();
+}
+
+void tablaOps_destruir(TablaOps tabla)
+{
+    glist_destruir(tabla, operador_destruir);
+}
+
+int tablaOps_vacia(TablaOps tabla)
+{
+    return glist_vacia(tabla);
+}
+
+TablaOps tablaOps_agregar_inicio(TablaOps tabla, Operador *data)
+{
+    return glist_agregar_inicio(tabla, (void*)data, operador_copiar);
+}
+
+int tablaOps_buscar(TablaOps tabla, Operador *data)
+{
+    return glist_buscar(tabla, (void*)data, operador_comparar);
+}
+
+void tablaOps_recorrer(TablaOps tabla, FuncionVisitante visitar)
+{
+    glist_recorrer(tabla, visitar);
+}
+
+void cargar_operador(TablaOps *tabla, char *simbolo, int aridad,
+                     FuncionEvaluacion eval)
+{
+    Operador nuevo = operador_crear(simbolo, evaluacion, eval);
+    
+    *tabla = tablaOps_agregar_inicio(*tabla, nuevo);
+    
+    operador_destruir(nuevo);
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tablaops.h
@@ -0,0 +1,31 @@
+#ifndef __TABLAOPS_H__
+#define __TABLAOPS_H__
+
+#include "glist.h"
+
+/*
+ * Almacenaremos la tabla de operadores como una lista de Operador
+ * */
+typedef GList TablaOps;
+
+
+TablaOps tablaOps_crear();
+
+void tablaOps_destruir(TablaOps tabla);
+
+int tablaOps_vacia(TablaOps tabla);
+
+TablaOps tablaOps_agregar_inicio(TablaOps tabla, Operador *data);
+
+int tablaOps_buscar(TablaOps tabla, Operador *data);
+
+void tablaOps_recorrer(TablaOps tabla, FuncionVisitante visitar);
+
+void cargar_operador(TablaOps *tabla, char *simbolo, int aridad,
+                     FuncionEvaluacion eval);
+
+
+
+
+
+#endif /** __TABLAOPS_H__ */
