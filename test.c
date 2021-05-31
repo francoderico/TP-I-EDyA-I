e6b3142e6a40e03d13b9c1381f24b1a075bc058e
francoderico <francoderico2001@gmail.com>
2021-05-30 21:28:33 -0300

    Pruebo que las funciones de struct_operador funcionen bien

--- test.c
+++ test.c
@@ -1,24 +1,39 @@
-#include "glist.h"
-#include "operacion.h"
-#include "operadores.h"
+//#include "glist.h"
+//#include "operacion.h"
+//#include "operadores.h"
 #include "struct_operador.h"
-#include "tablaops.h"
+//#include "tablaops.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 
+int suma(int *args)
+{
+    return args[0] + args[1];
+}
+
 
 
 int main(){
     
+    char* sim = malloc(sizeof(char) * 2);
     
-    TablaOps tabla = tablaOps_crear();
+    sim[0] = '+'; sim[1] = '\0';
     
-    cargar_operador(&tabla, '+', 2, suma);
+    Operador* nuevo = operador_crear(sim, 2, suma);
     
+    free(sim);
     
-    tablaOps_destruir(tabla);
+    int* a = malloc(sizeof(int) * 2);
+    
+    a[0] = 5; a[1] = 10;
+    
+    printf("%d\n", nuevo->eval(a));
+    
+    free(a);
+        
+    operador_destruir(nuevo);
     
     return 0;
 }
