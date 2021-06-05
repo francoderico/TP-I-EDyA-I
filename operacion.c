81f2c2a72d01dfb730b225a664709f7bf770e5dc
milagrososimi <67013413+milagrososimi@users.noreply.github.com>
2021-06-04 22:54:10 -0300

    quite lo que no iba

--- operacion.c
+++ operacion.c
@@ -9,21 +9,6 @@
 /*
  * Crea una nueva operación con los parámetros dados.
  * */
- 
-/*
-Operacion *operacion_crear(char *alias, BTree arbol, int resultado, FuncionCopia copia) {
-    Operacion *nuevo = malloc(sizeof(Operacion));
-
-    nuevo->alias = malloc(sizeof(char) * (strlen(alias) + 1));
-    strcpy(nuevo->alias, alias);
-
-    nuevo->arbol = btree_copiar(arbol, copia);
-
-    nuevo->resultado = resultado;
-
-    return nuevo;
-}
-* */
 
 
 Operacion* operacion_crear(char *alias, char *expr, TablaOps tabla)
@@ -52,16 +37,6 @@ void operacion_destruir(Operacion *op)
 
 
 /*
- * Crea una copia física de la operación.
- */
-//Operacion *operacion_copiar(Operacion * op, FuncionCopia copia2) {
-    //Operacion *copia = operacion_crear(op->alias, op->arbol, op->resultado, copia2);
-
-    //return copia;
-//}
-
-
-/*
  * Dadas 2 operaciones, las compara según su alias, siguiendo el mismo
  * criterio que la función strcmp: devuelve
  * -1 si op1->alias < op2->alias,
@@ -75,16 +50,3 @@ int operacion_comparar(Operacion * op1, Operacion * op2) {
 
 
 
-/*
- * Destruye la operación, liberando la memoria dinámica.
- * */
- 
-/*
-void operacion_destruir(Operacion * op, FuncionDestructora2 destruir) {
-    free(op->alias);
-
-    btree_destruir(op->arbol, destruir);
-
-    free(op);
-}
-*/
